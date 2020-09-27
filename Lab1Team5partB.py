450605c538c25ff57f6b71e47754be2f8c709fa3
Chinmay <chinmaysharma45@gmail.com>
2020-09-27 13:17:52 +0530

    create functions

--- Lab1Team5partB.py
+++ Lab1Team5partB.py
@@ -3,25 +3,40 @@
 # Sahib 
 
 import random
+import os
 
-fileName = "random1M.txt"
+fileName = "random.txt"
 
 random.seed(10)
 
-f = open(fileName, "w")
-print("Writing 1 Million file ...")
-for i in range(100):
-    line = str(random.randint(1,99))
-    f.write(line + '\n')
-    # print(line) 
-f.close()
+def fileWrite():
+    f = open(fileName, "w")
+    print("Writing 1 Million file ...")
+    for i in range(100):
+        line = str(random.randint(1,99))
+        f.write(line + '\n')
+        # print(line) 
+    f.close()
 
-print("\nReading and sorting 1 Million file ..." )
-readNums = []
-f = open(fileName,'r')
-for line in f:
-    # print(int(line))
-    readNums.append(int(line))
-f.close()
+def sortSmallArr(arr):
+    return sorted(arr)
 
-print(sorted(readNums))
+def readAndSplitFile():
+    print("\nReading and split 1 Million file ..." )
+    readNums1000 = []
+    f = open(fileName,'r')
+    counter = 0
+    fileCounter = 0
+    for line in f:
+        # print(int(line))
+        if (counter % 10 == 0 and counter != 0):
+            fx = open('file' + str(fileCounter) + '.txt','w')
+            for i in readNums1000:
+                fx.write(str(i) + '\n')
+            readNums1000.clear()
+        readNums1000.append(int(line))
+        counter+=1
+    f.close()
+    
+
+readAndSplitFile()
