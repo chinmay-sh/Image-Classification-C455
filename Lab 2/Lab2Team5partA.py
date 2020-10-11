82158a57e2baf2bf32bd88f1cd96a5b981d99788
Chinmay Sharma <chinmaysharma45@gmail.com>
2020-10-11 22:14:14 +0530

    add jaccard generator for partA

--- Lab 2/Lab2Team5partA.py
+++ Lab 2/Lab2Team5partA.py
@@ -7,4 +7,10 @@ A = {19,2,14,16,19,1,7,15,16,9,21,6,2,17,16,11,3,8,24}
 
 B = {12,2,14,5,20,11,13,14,10,2982,9,24,16,22,110,21,12,5,15}
 
-print(A)
+print(len(A))
+
+def genJaccard(setA,setB):
+    # return (len(setA & setB) / len(setA | setB))
+    return (len(setA.intersection(setB)) / len(setA.union(setB)))
+
+print(genJaccard(A,B))
