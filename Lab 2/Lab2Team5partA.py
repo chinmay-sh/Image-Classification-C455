bb87c997015a8f4a1c49c0e61d02000f86f12e98
Chinmay Sharma <chinmaysharma45@gmail.com>
2020-10-15 12:06:54 +0530

    update partA for empty sets

--- Lab 2/Lab2Team5partA.py
+++ Lab 2/Lab2Team5partA.py
@@ -11,6 +11,8 @@ B = {12,2,14,5,20,11,13,14,10,2982,9,24,16,22,110,21,12,5,15}
 
 def genJaccard(setA,setB):
     # return (len(setA & setB) / len(setA | setB))
+    if(len(setA) == 0 and len(setB) == 0):
+        return 1
     return (len(setA.intersection(setB)) / len(setA.union(setB)))
 
 print("Jaccard Similarity Value:", genJaccard(A,B))
